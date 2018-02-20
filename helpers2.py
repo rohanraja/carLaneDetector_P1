f05f673898a82109b4671125724ab4514b06c3ba
Rohan Raja <rohan@Rohans-MacBook-Pro.local>
2018-02-20 22:33:05 +0530

    Clearn up

--- helpers2.py
+++
@@ -1,2 +0,0 @@
-def loadImg():
-    return None
--- test_fitPoly.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestFitPolynomial(unittest.TestCase):
-    def test_fit_polynomial(self):
-        # self.assertEqual(expected, fitPolynomial())
-        assert False # TODO: implement your test here
-
-if __name__ == '__main__':
-    unittest.main()
